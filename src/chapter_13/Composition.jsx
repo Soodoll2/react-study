b90d1940debe5b73b4217f5ada17cf7621af4e3f
soodoll2-notebook <qiipvqiip@gmail.com>
2023-11-27 00:23:13 +0900

    chapter13

--- src/chapter_13/Composition.jsx
+++ src/chapter_13/Composition.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 
 /// [1] Containment : 하위 컴포넌트를 포함하는 합성 방법 
 // 1) 하나의 chlidren 사용
 // Dialog > FancyBorder
 function FancyBorder(props) {
   return(
-    <div className={'Fancyborder FancyBorder-' + props.color}>
+    <div style={{borderWidth: "10px", borderStyle: "solid",borderColor: props.color || "white",}}>
       {props.children}
     </div>
   );
 }
 // 2) 여러개의 chlidren 집합이 필요한 경우 별도의 props를 정의해서 사용 
 // Dialog > SplitPane (props.left,props.right)
+function Contacts() {}
+function Chat() {}
 function SplitPane(props) {
   return (
     <div className="SplitPane">
@@ -35,23 +38,44 @@ function App() {
 function Dialog(props){
   return (
     <FancyBorder color="pink">
-      {/* FancyBorder 컴포넌트의 props.children */}
       <h1 className="Dialog-title">{props.title}</h1>
       <p className="Dialog-message">{props.message}</p>
+      {/* Containment : 하위 컴포넌트를 포함하는 합성 방법  */}
+      {props.children}
     </FancyBorder>
   );
 }
 // 특수화 시킨 컴포넌트
 function WelcomeDialog(props){
   return (
+    <>
+    {/* Specialization  */}
     <Dialog title="어서오세요" message="우리 사이트에 방문하신 것을 환영합니다!" />
+    </>
   );
 }
 
 // [3] Containment + Specialization
+// Specialization : 화성 탐사 프로그램 닉네임 입력 특수화 컴포넌트
+function SignUpDialog(props) {
+  const [nickname, setNickname] = useState('');
 
+  const handleChange = (event) => {
+    setNickname(event.target.value);
+  }
+
+  const handleSignUp = () => {
+    alert(`어서 오세요, ${nickname}님!`)
+  }
+
+  return (
+    <Dialog title="화성 탐사 프로그램" message="닉넴임을 입력해주세요.">
+      {/* Dialog 의 props.children */}
+      <input value={nickname} onChange={handleChange} />
+      <button onClick={handleSignUp}>가입하기</button>
+    </Dialog>
+  );
+}
+export default SignUpDialog;
 
 // [4] 상속 : 리액트에선 상속 사용 X / 컴포넌트의 조합으로 새로운 컴포넌트 만들기!
-
-
-//
--- src/chapter_13/ProfileCard.jsx
+++ src/chapter_13/ProfileCard.jsx
@@ -2,9 +2,9 @@ import Card from "./Card";
 
 function ProfileCard(props) {
     return (
-        <Card title="Inje Lee" backgroundColor="#4ea04e">
-            <p>안녕하세요, 소플입니다.</p>
-            <p>저는 리액트를 사용해서 개발하고 있습니다.</p>
+        <Card title="SOODOLL" backgroundColor="#4ea04e">
+            <p>안녕? 나는 소플의 책으로 리액트를 공부하는 수돌이야.</p>
+            <p>소플... 두고보자</p>
         </Card>
     );
 }
